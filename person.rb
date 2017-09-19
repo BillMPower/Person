1a191f5659fe300ca71f33bf6fa86f8ad2673fd2
Bill Power <bill.power@ucdconnect.ie>
2017-09-19 09:35:40 +0100

    first person file commit

---
+++ person.rb
@@ -0,0 +1,31 @@
+class Person
+
+  def initialize(name)
+    @name = name
+    @age = 0
+  end
+
+  def to_s
+    puts "#{@name} is #{@age} years old"
+  end
+
+  def increment_age
+    @age+=1
+    if @age == 13
+      puts "#{@name} is a teenager"
+    end
+    if @age == 18
+      puts "#{@name} is an adult"
+    end
+  end
+
+end
+
+my_person = Person.new('Bill')
+my_person.to_s
+
+20.times do
+  my_person.increment_age
+end
+
+my_person.to_s
